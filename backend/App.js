2f4f819497dc6cef3d27f9a75fff9587d7bf9148
BADEGE DIVYA <137250607+DIVYA8500@users.noreply.github.com>
2024-09-30 17:19:41 +0530

    Add files via upload

---
+++ backend/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from './components/RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SmokeTrees User Registration</h1>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
